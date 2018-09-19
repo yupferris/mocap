65b59164ce09ea2d47873a839604a2277ff35c12
ferris <yupferris@gmail.com>
2018-09-19 20:50:22 +0200

    Round-trip bvh->mocap->bvh

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct Mocap {
 
 #[derive(Debug)]
 struct Joint {
+    name: String,
     offset: (f32, f32, f32),
     channels: Vec<Channel>,
     children: JointChildren,
@@ -58,6 +59,7 @@ fn build_mocap(bvh: &bvh::Bvh) -> Mocap {
 
 fn build_joint(bvh_joint: &bvh::Joint) -> Joint {
     Joint {
+        name: bvh_joint.name.clone(),
         offset: (bvh_joint.offset.x as _, bvh_joint.offset.y as _, bvh_joint.offset.z as _),
         channels: bvh_joint.channels.iter().map(|channel| Channel {
             type_: match channel {
@@ -94,34 +96,56 @@ fn pull_frame_data(joint: &mut Joint, frames: &Vec<Vec<f64>>, frame_index: &mut 
     }
 }
 
-fn main() {
-    let input_file_name = args().nth(1).unwrap();
-    let output_file_name = args().nth(2).unwrap();
-    let csv_file_name = args().nth(3).unwrap();
+fn build_bvh(mocap: &Mocap) -> bvh::Bvh {
+    let mut frames = vec![Vec::new(); mocap.num_frames as usize];
 
-    let input = {
-        let mut ret = String::new();
-        let mut file = File::open(input_file_name).unwrap();
-        file.read_to_string(&mut ret).unwrap();
-        ret
-    };
+    bvh::Bvh {
+        hierarchy: bvh::Hierarchy {
+            root: build_bvh_joint(&mocap.root, &mut frames),
+        },
+        motion: bvh::Motion {
+            num_frames: mocap.num_frames,
+            frame_time: mocap.frame_time as _,
+            frames: frames,
+        },
+    }
+}
 
-    let bvh = bvh::parse(&input).unwrap();
+fn build_bvh_joint(joint: &Joint, frames: &mut Vec<Vec<f64>>) -> bvh::Joint {
+    let mut channels = Vec::new();
+    for channel in joint.channels.iter() {
+        channels.push(match channel.type_ {
+            ChannelType::TranslationX => bvh::Channel::XPosition,
+            ChannelType::TranslationY => bvh::Channel::YPosition,
+            ChannelType::TranslationZ => bvh::Channel::ZPosition,
+            ChannelType::RotationX => bvh::Channel::XRotation,
+            ChannelType::RotationY => bvh::Channel::YRotation,
+            ChannelType::RotationZ => bvh::Channel::ZRotation,
+        });
 
-    // TODO: Quantize bvh parameters
-
-    {
-        let mut output = File::create(output_file_name).unwrap();
-        bvh::serialize(&bvh, &mut output).unwrap();
+        for (index, value) in channel.values.iter().enumerate() {
+            frames[index].push(*value as _);
+        }
     }
 
-    let mocap = build_mocap(&bvh);
+    bvh::Joint {
+        name: joint.name.clone(),
+        offset: build_bvh_offset(&joint.offset),
+        channels: channels,
+        children: match joint.children {
+            JointChildren::Joints(ref joints) => bvh::JointChildren::Joints(joints.iter().map(|joint| build_bvh_joint(joint, frames)).collect()),
+            JointChildren::EndSite(ref offset) => bvh::JointChildren::EndSite(bvh::EndSite {
+                offset: build_bvh_offset(offset),
+            }),
+        },
+    }
+}
 
-    //println!("Result: {:#?}", mocap);
-
-    {
-        let mut csv = File::create(csv_file_name).unwrap();
-        dump_channels(&mocap.root, &mut csv).unwrap();
+fn build_bvh_offset(offset: &(f32, f32, f32)) -> bvh::Offset {
+    bvh::Offset {
+        x: offset.0 as _,
+        y: offset.1 as _,
+        z: offset.2 as _,
     }
 }
 
@@ -139,4 +163,32 @@ fn dump_channels<W: Write>(joint: &Joint, w: &mut W) -> io::Result<()> {
     }
 
     Ok(())
+}
+
+fn main() {
+    let input_file_name = args().nth(1).unwrap();
+    let output_file_name = args().nth(2).unwrap();
+    let csv_file_name = args().nth(3).unwrap();
+
+    let input = {
+        let mut ret = String::new();
+        let mut file = File::open(input_file_name).unwrap();
+        file.read_to_string(&mut ret).unwrap();
+        ret
+    };
+
+    let bvh = bvh::parse(&input).unwrap();
+    let mocap = build_mocap(&bvh);
+    //println!("Result: {:#?}", mocap);
+
+    {
+        let bvh = build_bvh(&mocap);
+        let mut output = File::create(output_file_name).unwrap();
+        bvh::serialize(&bvh, &mut output).unwrap();
+    }
+
+    {
+        let mut csv = File::create(csv_file_name).unwrap();
+        dump_channels(&mocap.root, &mut csv).unwrap();
+    }
 }
