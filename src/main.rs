5fb4d8bb8bda4a3ea1c9435096029556e44679d2
ferris <yupferris@gmail.com>
2018-09-19 10:44:07 +0200

    Test serialization and update after bvh format changes

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,10 @@ fn build_mocap(bvh: &bvh::Bvh) -> Mocap {
         root: build_joint(&bvh.hierarchy.root),
     };
 
+    let mut frame_index = 0;
     let mut frame_data_index = 0;
     for _ in 0..bvh.motion.num_frames {
-        pull_frame_data(&mut ret.root, &bvh.motion.frame_data, &mut frame_data_index);
+        pull_frame_data(&mut ret.root, &bvh.motion.frames, &mut frame_index, &mut frame_data_index);
     }
 
     ret
@@ -76,21 +77,27 @@ fn build_joint(bvh_joint: &bvh::Joint) -> Joint {
     }
 }
 
-fn pull_frame_data(joint: &mut Joint, frame_data: &[f64], frame_data_index: &mut usize) {
+fn pull_frame_data(joint: &mut Joint, frames: &Vec<Vec<f64>>, frame_index: &mut usize, frame_data_index: &mut usize) {
     for channel in joint.channels.iter_mut() {
-        channel.values.push(frame_data[*frame_data_index] as _);
+        channel.values.push(frames[*frame_index][*frame_data_index] as _);
         *frame_data_index += 1;
+        if *frame_data_index >= frames[*frame_index].len() {
+            *frame_data_index = 0;
+            *frame_index += 1;
+        }
     }
 
     if let JointChildren::Joints(ref mut joints) = joint.children {
         for joint in joints.iter_mut() {
-            pull_frame_data(joint, frame_data, frame_data_index);
+            pull_frame_data(joint, frames, frame_index, frame_data_index);
         }
     }
 }
 
 fn main() {
     let input_file_name = args().nth(1).unwrap();
+    let output_file_name = args().nth(2).unwrap();
+
     let input = {
         let mut ret = String::new();
         let mut file = File::open(input_file_name).unwrap();
@@ -99,6 +106,14 @@ fn main() {
     };
 
     let bvh = bvh::parse(&input).unwrap();
+
+    // TODO: Quantize bvh parameters
+
+    {
+        let mut output = File::create(output_file_name).unwrap();
+        bvh::serialize(&bvh, &mut output).unwrap();
+    }
+
     let mocap = build_mocap(&bvh);
 
     println!("Result: {:#?}", mocap);
